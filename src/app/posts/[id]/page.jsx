c1b9f9eedb49b392d417c41ad1d77f7908b7a31f
Muhammad Asif <muhammad.asif@ceative.co.uk>
2023-10-04 00:17:18 -0700

    localstorage changes reverted

--- src/app/posts/[id]/page.jsx
+++ src/app/posts/[id]/page.jsx
@@ -1,19 +1,16 @@
-"use client";
 import React from "react";
 
-const PostDetails = ({ params }) => {
-  const getLs = JSON.parse(localStorage.getItem("posts"));
-  const currPost = getLs
-    ? getLs.find((obj) => obj.id === params.id)
-    : { id: 1, title: "this is title" };
-
+const PostDetails = async ({ params }) => {
+  const post = await fetch(`https://dummyjson.com/posts/${params?.id}`).then((data) =>
+    data?.json()
+  );
   return (
     <div className="flex min-h-screen flex-col items-center pt-24 px-40">
       <h1 className="text-3xl font-semibold">Blog Post Details</h1>
       <div style={{ paddingTop: "50px" }}>
-        <article key={currPost?.id}>
-          <h2 className="text-2xl font-semibold mb-2">{currPost?.id}</h2>
-          <p style={{ paddingBottom: "30px" }}>{currPost?.title}</p>
+        <article key={post?.id}>
+          <h2 className="text-2xl font-semibold mb-2">{post?.title}</h2>
+          <p style={{ paddingBottom: "30px" }}>{post?.body}</p>
         </article>
       </div>
     </div>
